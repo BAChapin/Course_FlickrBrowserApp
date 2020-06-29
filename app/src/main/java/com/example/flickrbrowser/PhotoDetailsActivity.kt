d4138e2679aca7db6336f0b2c678aa01523039e1
BAChapin <brett.chapin89@gmail.com>
2020-06-29 10:46:39 -0400

    Removed warnings from browse.xml and content_photo_details.xml Added some string resources to strings.xml Updated how I was displaying the photo's tags and title using the newly created resources

--- app/src/main/java/com/example/flickrbrowser/PhotoDetailsActivity.kt
+++ app/src/main/java/com/example/flickrbrowser/PhotoDetailsActivity.kt
@@ -15,8 +15,8 @@ class PhotoDetailsActivity : BaseActivity() {
 
         val photo = intent.getParcelableExtra(PHOTO_TRANSFER) as Photo
 
-        photo_title.text = photo.title
-        photo_tags.text = photo.tags
+        photo_title.text = resources.getString(R.string.photo_title_text, photo.title)
+        photo_tags.text = resources.getString(R.string.photo_tags_text, photo.tags)
         photo_author.text = photo.author
 
         Picasso.with(this).load(photo.link)
